aad818c7c8af02e032f4656b48bab8ffe7dae0ed
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-07-18 15:16:49 +0530

    Added code to create, show and use payroll_service database

--- src/main/java/com/bridgelabz/jdbc/PayrollServiceJDBC.java
+++ src/main/java/com/bridgelabz/jdbc/PayrollServiceJDBC.java
@@ -1,8 +1,25 @@
 package com.bridgelabz.jdbc;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class PayrollServiceJDBC {
     public static void main(String[] args) {
-        System.out.println("Payroll service JDBC");
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://root@localhost:3306/payroll_service", "root", "root");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("show databases");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
