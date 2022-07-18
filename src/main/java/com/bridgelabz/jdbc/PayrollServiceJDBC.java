dd2d35f547fe9bd889d1fc4b59eeb53d0f1741a9
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-07-18 15:33:58 +0530

    Added code to create table employee_payroll  in payroll_service database

--- src/main/java/com/bridgelabz/jdbc/PayrollServiceJDBC.java
+++ src/main/java/com/bridgelabz/jdbc/PayrollServiceJDBC.java
@@ -12,11 +12,14 @@ public class PayrollServiceJDBC {
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://root@localhost:3306/payroll_service", "root", "root");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("show databases");
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
-
+            String sql =  "CREATE TABLE employee_payroll " +
+                    "(id int unsigned not null auto_increment, " +
+                    " name VARCHAR(255), " +
+                    " salary Double, " +
+                    " Start_Date date, " +
+                    " PRIMARY KEY ( id ))";
+            stmt.executeUpdate(sql);
+            System.out.println("Created table in given database...");
             con.close();
         } catch (Exception e) {
             System.out.println(e);
